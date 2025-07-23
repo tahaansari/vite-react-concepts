0d3b5992aa5d0b5fc967703a5830e537c2fca8ef
Taha Ansari <taha.m.ansari@gmail.com>
2025-07-23 16:45:55 +0530

    wip

--- src/ParentComponent.jsx
+++ src/ParentComponent.jsx
@@ -5,33 +5,32 @@ import ChildComponent from "./ChildComponent";
 const ParentComponent = () => {
     console.log('parent component rendered')
     const [count, setCount] = useState(0);
-    const [name,setName] = useState('');
-    // const [memorizedData,setMemorizedData] = useState(0);
+    const [name,setName] = useState('Taha Ansari');
+    const [confirmedName,setConfirmedName] = useState('');
     const changeCount = () => {
         setCount(count + 1)
     }
     const memorizedDataFun = useMemo(() => {
         console.log('memorizedDataFun re-rendered')
         return { name: name }
-    }, [name])
+    }, [confirmedName])
 
     const changeMemorizedDataFun = ()=>{
-        console.log("update the memorizedDataFun")   
-        // setName(e.target.value)
+        console.log("update the memorizedDataFun")  
+        setConfirmedName(name);
     }
     
     return <>
         <div style={{ border: "1px solid red" }}>
             <div style={{padding:"20px"}}>
                 <h1>Parent Component</h1>
-                <h1>count - {count}</h1>
-                <input type="text"  value={name} placeholder="enter you name" />
+                <h1>count - {count} <button onClick={changeCount}>increment count</button></h1>
+                <input type="text"  value={name} onChange={(e)=>setName(e.target.value)} placeholder="enter you name" />
                 <button onClick={changeMemorizedDataFun}>change name</button>
-                <button onClick={changeCount}>increment count</button>
             </div>
             <ChildComponent data={memorizedDataFun} />
         </div>
     </>
 }
 
-export default ParentComponent; 
+export default ParentComponent;
